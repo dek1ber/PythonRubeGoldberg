de5105bf756c0c95ade28e73894e9fb6a36c565d
KevinKusiak <40304339+KevinKusiak@users.noreply.github.com>
2019-10-29 20:56:51 -0500

    Pulley

--- game.py
+++ game.py
@@ -17,6 +17,43 @@ def draw_collision(arbiter, space, data):
         p = pymunk.pygame_util.to_pygame(c.point_a, data["surface"])
         pygame.draw.circle(data["surface"], THECOLORS["black"], p, r, 1)
 
+def add_L(space):
+    # """Add a inverted L shape with two joints"""
+    # rotation_center_body = pymunk.Body(body_type = pymunk.Body.STATIC)
+    # rotation_center_body.position = (300,300)
+    #
+    # rotation_limit_body = pymunk.Body(body_type = pymunk.Body.STATIC)
+    # rotation_limit_body.position = (200,300)
+    #
+    # body = pymunk.Body(10, 10000)
+    # body.position = (250,300)
+    # l1 = pymunk.Segment(body, (-50, 200), (100.0, 200.0), 1)
+    # l2 = pymunk.Segment(body, (100.0, 200), (100.0, 250.0), 1)
+    #
+    # rotation_center_joint = pymunk.PinJoint(body, rotation_center_body, (0,0), (0,0))
+    # joint_limit = 125
+    # rotation_limit_joint = pymunk.SlideJoint(body, rotation_limit_body, (-50,0), (0,0), 0, joint_limit)
+    #
+    # space.add(l1, l2, body, rotation_center_joint, rotation_limit_joint)
+    # return l1,l2
+    rotation_center_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+    rotation_center_body.position = (300, 300)
+
+    rotation_limit_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+    rotation_limit_body.position = (200, 300)
+
+    body = pymunk.Body(10, 10000)
+    body.position = (300, 300)
+    l1 = pymunk.Segment(body, (-100, 125), (100.0, 125.0), 1)
+    l2 = pymunk.Segment(body, (100.0, 125), (100.0, 150.0), 1)
+
+    rotation_center_joint = pymunk.PinJoint(body, rotation_center_body, (0, 25), (0, 25))
+    joint_limit = 200
+    rotation_limit_joint = pymunk.SlideJoint(body, rotation_limit_body, (-150, 125), (0, 125), 0, joint_limit)
+
+    space.add(l1, l2, body, rotation_center_joint, rotation_limit_joint)
+    return l1, l2
+
 
 def main():
     global contact
@@ -34,13 +71,50 @@ def main():
     # disable the build in debug draw of collision point since we use our own code.
     draw_options.flags = draw_options.flags ^ pymunk.pygame_util.DrawOptions.DRAW_COLLISION_POINTS
     ## Balls
+
+    lines = add_L(space)
+
     balls = []
 
     ### walls
-    static_lines = [pymunk.Segment(space.static_body, (10.0, 400.0), (150.0, 350.0), 5)]
+    static_lines = [pymunk.Segment(space.static_body, (10.0, 575.0), (150.0, 475.0), 5)]
     for l in static_lines:
         l.friction = 0.5
     space.add(static_lines)
+
+    rotation_center_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+    rotation_center_body.position = (300, 300)
+
+    rotation_limit_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+    rotation_limit_body.position = (300, 300)
+
+    # skiLift = [pymunk.Segment(space.static_body, (300.0, 500.0), (450.0, 475.0), 5.0), pymunk.Segment(space.static_body, (450.0, 475.0), (450.0, 525.0), 5.0)]
+
+    # for l in skiLift:
+    #     l.friction = 0.5
+    # space.add(skiLift)
+
+
+    body = pymunk.Body(10, 10000)
+    body.position = (300, 300)
+    # l1 = pymunk.Segment(rotation_limit_body, (25, -20), (25, 100.0), 1.0)
+    # l2 = pymunk.Segment(rotation_limit_body, (-25, -20), (-25, 100.0), 1.0)
+    #
+    # l3 = pymunk.Segment(rotation_limit_body, (25, -20), (85.0, -100.0), 1.3)
+    # l4 = pymunk.Segment(rotation_limit_body, (-25, -20), (-85.0, -100.0), 1.3)
+    # # l8 = pymunk.Segment(body, (-150, -200), (150.0, -200.0), 5.0)
+    # #
+    # # rotation_center_joint = pymunk.PinJoint(body, rotation_center_body, (0,-200), (0,-200))
+    # # joint_limit = 25
+    # # rotation_limit_joint = pymunk.SlideJoint(body, rotation_limit_body, (-100,-200), (0,-200), 0, joint_limit)
+    # l8 = pymunk.Segment(body, (-100, -200), (100.0, -200.0), 5.0)
+    #
+    # rotation_center_joint = pymunk.PinJoint(body, rotation_center_body, (0, -200), (0, -200))
+    # joint_limit = 1
+    # rotation_limit_joint = pymunk.SlideJoint(body, rotation_limit_body, (-20, -200), (0, -200), 0, joint_limit)
+    #
+    # space.add(l1, l2, l3, l4, l8, rotation_limit_body, rotation_center_body, body, rotation_center_joint,
+    #           rotation_limit_joint)
 
     ticks_to_next_ball = 1
 
@@ -59,13 +133,13 @@ def main():
 
         ticks_to_next_ball -= 1
         if ticks_to_next_ball == 0:
-            ticks_to_next_ball = 100
-            mass = 1
+            ticks_to_next_ball = 0
+            mass = 0.5
             radius = 15
             inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
             body = pymunk.Body(mass, inertia)
             # x = random.randint(50, 200)
-            body.position = 30, 413
+            body.position = 30, 650
             shape = pymunk.Circle(body, radius, (0, 0))
             shape.friction = 0.5
             space.add(body, shape)
@@ -85,12 +159,31 @@ def main():
         # Make body for pulley
 
         # Update physics
+        ## Balls
 
-        ### Flip screen
+        ### walls
+
+        ### Draw stuff
+        # balls_to_remove = []
+        # for ball in balls:
+        #     if ball.body.position.y > 400: balls_to_remove.append(ball)
+        #     p = tuple(map(int, ball.body.position))
+        #     pygame.draw.circle(screen, THECOLORS["blue"], p, int(ball.radius), 2)
+        #
+        # for ball in balls_to_remove:
+        #     space.remove(ball, ball.body)
+        #     balls.remove(ball)
+        #
+        # for line in static_lines:
+        #     body = line.body
+        #     p1 = body.position + line.a.rotated(body.angle)
+        #     p2 = body.position + line.b.rotated(body.angle)
+        #     pygame.draw.lines(screen, THECOLORS["lightgray"], False, [p1, p2])
+
+        ## Flip screen
         pygame.display.flip()
         clock.tick(50)
         pygame.display.set_caption("Rube Goldberg")
-
 
 
 if __name__ == '__main__':
